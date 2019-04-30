7ebff59ac6290f41f5077c04bd067f97d532af85
Camille <camille.clarablaix@gmail.com>
2019-04-30 17:19:47 +0200

    deernier commit
    dernier commit camille blaix

--- io/io.js
+++ io/io.js
@@ -1,5 +1,12 @@
 module.exports.decodeHexFileContent = (filePath) =>  {
     return new Promise((resolve, reject) => {
-        // To be implemented!
+        const fs = require('fs'); 
+        module.exports.decodeHexFileContent = (filePath) => { 
+        	return new Promise((resolve, reject) => { 
+        		if(filePath){ const encodedFile = fs.readFileSync(filePath, 'utf8'); 
+        		if(encodedFile){ const decoded = Buffer.from(encodedFile, 'hex').toString('utf8'); 
+        		if(decoded){ resolve(decoded); } } } reject(); 
+        	}); 
+        }
     });
 }
