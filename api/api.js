b7c4c2ccdb31d0c7723a70f31175cba9e7a7b988
Camille <camille.clarablaix@gmail.com>
2019-04-30 16:47:35 +0200

    recomit
    re exo 1 et 2

--- api/api.js
+++ api/api.js
@@ -5,7 +5,6 @@ const peopleService = new PeopleService();
 const app = express();
 
 // V1
- 
 // Express a besoin d’un middleware pour parser les corps de requêtes POST
 const bodyParser = require('body-parser');
  
@@ -14,13 +13,19 @@ const { digestAuth } = require('../basic-auth/basic-auth');
  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// app.use('/api/v1', v1);
  
-app.get('/people', async (request, response) => {
-    const data = await peopleService.getPeople();
-    response.send(data);
+app.get('/api/v1/people', async (request, response) => {
+    try {
+        const data = await peopleService.getPeople();
+        response.send(data);
+    } catch (error) {
+        console.log('error get people: ', error);
+        response.sendStatus(404).end(error);
+    }
 });
  
-app.put('/people/:id', async (request, response) => {
+app.put('/api/v1/people/:id', async (request, response) => {
     const id = request.params.id;
     const people = {...request.name };
     try {
@@ -28,7 +33,7 @@ app.put('/people/:id', async (request, response) => {
         response.sendStatus(200);
     } catch (error) {
         console.log('error update people: ', error);
-        response.sendStatus(400).end(error);
+        response.sendStatus(404).end(error);
     }
 });
 
